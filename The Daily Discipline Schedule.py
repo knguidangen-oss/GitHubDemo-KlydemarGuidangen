916c7108cc1cbd7d59b6bae196cbac050b989d3c
knguidangen-oss <knguidangen@carc.pshs.edu.ph>
2026-02-16 06:34:59 +0800

    The Daily Discipline Schedule - MENU LAYOUT

---
+++ The Daily Discipline Schedule.py
@@ -0,0 +1,10 @@
+def menu ():
+    print("\n == Main Menu ==")
+    print("1. The table of activities")
+    print("2. Finished activities")
+    print("3. Add a new activity to the list")
+    print("4. Remove an activity from the list")
+    print("5. Exit the program")
+    print("="*10)
+
+menu ()
